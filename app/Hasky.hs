762e51a8e1281a45705be3dde961459fcd627b7f
LaMavia <ts438730@students.mimuw.edu.pl>
2024-04-08 21:40:49 +0200

    add Hasky main

--- app/Hasky.hs
+++ app/Hasky.hs
@@ -1,4 +1,62 @@
-import Runtime.RTState
+module Main where
+
+import Prelude
+  ( ($), (.)
+  , Either(..)
+  , Int, (>)
+  , String, (++), concat, unlines
+  , Show, show
+  , IO, (>>), (>>=), mapM_, putStrLn
+  , FilePath
+  , getContents, readFile
+  )
+import System.Environment ( getArgs )
+import System.Exit        ( exitFailure )
+import Control.Monad      ( when )
+
+import Abs   ( Program )
+import Lex   ( Token, mkPosToken )
+import Par   ( pProgram, myLexer )
+import Print ( printTree )
+import Skel  ()
+import Preprocessor.Desugar (desugarProgram)
+
+type Err        = Either String
+type ParseFun   = [Token] -> Err Program
+type Verbosity  = Int
+
+putStrV :: Verbosity -> String -> IO ()
+putStrV v s = when (v > 1) $ putStrLn s
+
+runFile :: Verbosity -> ParseFun -> FilePath -> IO ()
+runFile v p f = putStrLn f >> readFile f >>= run v p
+
+run :: Verbosity -> ParseFun -> String -> IO ()
+run v p s =
+  case p ts of
+    Left err -> do
+      putStrLn "\nParse              Failed...\n"
+      putStrV v "Tokens:"
+      mapM_ (putStrV v . showPosToken . mkPosToken) ts
+      putStrLn err
+      exitFailure
+    Right tree -> do
+      putStrLn "\nParse Successful!"
+      showTree v . desugarProgram $ tree
+  where
+  ts = myLexer s
+  showPosToken ((l,c),t) = concat [ show l, ":", show c, "\t", show t ]
+
+showTree :: Int -> Program -> IO ()
+showTree v tree = do
+  putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
+  putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
 
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+  case args of
+    []         -> getContents >>= run 2 pProgram
+    "-s":fs    -> mapM_ (runFile 0 pProgram) fs
+    fs         -> mapM_ (runFile 2 pProgram) fs
+
